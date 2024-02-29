709f8babe45a4378d2b2e4ba5727fea596879179
novica <nnovica@gmail.com>
2024-02-29 12:44:17 +0100

    feat: move about to a separate md file

--- app.R
+++ app.R
@@ -204,54 +204,7 @@ ui <- fluidPage(
                  
                  column(width = 9,
                         
-                 h4("Datakilder"),
-                 
-                 p("Tap av laksefisk gjennom produksjonen i sjø fra utsett til slakting som er rapportert til Fiskeridirektoratet per januar 2024. 
-                 Tapene er fordelt på dødfisk, utkast, rømming og «annet». Dødfisk omfatter dødelighet som skyldes sykdom og skader osv. 
-                   Dødfisk omfatter dødelighet som skyldes sykdom og skader osv.
-                   Smittsomme sykdommer er en av de viktigste biologiske og økonomiske
-                   tapsfaktorene i fiskeoppdrett. Utkast er skrapfisk som sorteres ut ved slakting.
-                   «Annet» kan omfatte dødelighetsepisoder som oppstår ved lusebehandling og annen håndtering,
-                   men også fisk som avlives i forbindelse med sykdomsbekjempelse."),
-                 
-                 p("Det er disse tallene som ligger til grunn for tabellene presentert her."),
-                 
-                 h4("Databearbeiding"),
-                 
-                 p("Det finnes to ulike tabeller, der tallene for ‘Tap’ og ‘Dødelighet’ er beregnet og presentert på ulikt vis:"), 
-                 
-                 p("I beregningene av ‘Tap’ inngår data fra all sjøsatt laks og regnbueørret, inklusive matfisk, stamfisk,
-                   fisk fra forsknings- og utviklingskonsesjoner, undervisningskonsesjoner med flere.
-                   Disse tallene viser hvor mange fisk som har dødd, blitt kassert, rømt osv per år.
-                   Prosentene viser til hvor stor andel den pågjeldende kategorien utgjør av det
-                   totale tapet (for eksempel hvor stor andel av tapet som utgjøres av dødelighet, utkast eller rømming)."),
-                 
-                 p("I beregningene av ‘Dødelighet’ er prosent døde angitt for henholdsvis laks og regnbueørret.
-                   I disse tallene inngår ikke tap som følge av utkast, rømming eller «annet».
-                   Beregningene er foretatt ved bruk av rater, som tillater at populasjonen av
-                   fisk som kan dø endrer seg måned for måned. Først blir den månedlige dødsraten
-                   for hver lokalitet beregnet, og disse ratene blir deretter brukt til å beregne
-                   gjennomsnittet for hver måned. Dette gjennomsnittet blir til slutt summert og
-                   deretter konvertert til prosent dødfisk hvert år."),
-                 
-                 p("Geografisk område: Det er mulig å velge om en vil se tallene samlet for fylke,
-                 produksjonssone eller på nasjonalt nivå. Det benyttes gjeldende fylkesgrenser.
-                 For fylker som er slått sammen, presenteres historiske data med dagens fylkesgrenser.
-                 Produksjonssonene viser til de 13 produksjonssoner som er definert i forskrift om
-                 produksjonsområder (FOR-2017-01-16-61).Produksjonsområder eller fylker med meget få
-                 lokaliteter er tatt ut av fremstillingen, for at det ikke skal være mulig å kjenne igjen enkelt lokaliteter."),
-                 
-                 h4("Referanser"),
-                 
-                 p("Toft, N., Agger, J. F., Houe, H., & Bruun, J. (2004). Measures of disease frequency. In H. Houe, A. K. Ersbøll, & N. Toft (Eds.), Introduction to Veterinary Epidemiology (pp. 77–93). Frederiksberg, Denmark: Biofolia."),
-                 
-                 p("Bang Jensen, B., Qviller, L. & Toft, N. (2020). Spatio-temporal variations in mortality during the seawater production phase of Atlantic salmon (Salmo salar) in Norway. J. Fish Dis. 43, 445–457."),
-                 
-                 h4("Kontakt"),
-                 
-                 p("Dersom du har spørsmål eller kommentarer til tabellene, vennligst ta kontakt med  ",
-                   a(href = 'mailto:edgar.brun@vetinst.no', "Edgar Brun ", .noWS = "outside"),
-                   "(avdelingsdirektør, fiskehelse og fiskevelferd).")
+               shiny::includeMarkdown("www/about.md")
                  
                  )) 
       )
---
+++ www/about.md
@@ -0,0 +1,54 @@
+#### Datakilder
+                 
+Tap av laksefisk gjennom produksjonen i sjø fra utsett til slakting som er
+rapportert til Fiskeridirektoratet per januar 2024. Tapene er fordelt på
+dødfisk, utkast, rømming og «annet». Dødfisk omfatter dødelighet som skyldes
+sykdom og skader osv. Dødfisk omfatter dødelighet som skyldes sykdom og skader 
+osv. Smittsomme sykdommer er en av de viktigste biologiske og økonomiske 
+tapsfaktorene i fiskeoppdrett. Utkast er skrapfisk som sorteres ut ved slakting.
+«Annet» kan omfatte dødelighetsepisoder som oppstår ved lusebehandling 
+og annen håndtering, men også fisk som avlives i forbindelse med 
+sykdomsbekjempelse.
+                 
+Det er disse tallene som ligger til grunn for tabellene presentert her.
+                 
+#### Databearbeiding
+                 
+Det finnes to ulike tabeller, der tallene for ‘Tap’ og ‘Dødelighet’ er 
+beregnet og presentert på ulikt vis:
+                 
+I beregningene av ‘Tap’ inngår data fra all sjøsatt laks og regnbueørret, 
+inklusive matfisk, stamfisk, fisk fra forsknings- og utviklingskonsesjoner, 
+undervisningskonsesjoner med flere. Disse tallene viser hvor mange fisk som 
+har dødd, blitt kassert, rømt osv per år. Prosentene viser til hvor stor 
+andel den pågjeldende kategorien utgjør av det totale tapet 
+(for eksempel hvor stor andel av tapet som utgjøres av dødelighet, 
+utkast eller rømming).
+                 
+I beregningene av ‘Dødelighet’ er prosent døde angitt for henholdsvis laks og 
+regnbueørret.
+I disse tallene inngår ikke tap som følge av utkast, rømming eller «annet».
+Beregningene er foretatt ved bruk av rater, som tillater at populasjonen av
+fisk som kan dø endrer seg måned for måned. Først blir den månedlige dødsraten
+for hver lokalitet beregnet, og disse ratene blir deretter brukt til å beregne
+gjennomsnittet for hver måned. Dette gjennomsnittet blir til slutt summert og
+deretter konvertert til prosent dødfisk hvert år."),
+                 
+Geografisk område: Det er mulig å velge om en vil se tallene samlet for fylke,
+produksjonssone eller på nasjonalt nivå. Det benyttes gjeldende fylkesgrenser.
+For fylker som er slått sammen, presenteres historiske data med dagens fylkesgrenser.
+Produksjonssonene viser til de 13 produksjonssoner som er definert i forskrift om
+produksjonsområder (FOR-2017-01-16-61). Produksjonsområder eller fylker med meget få
+lokaliteter er tatt ut av fremstillingen, for at det ikke skal være mulig å kjenne 
+igjen enkelt lokaliteter.
+                 
+#### Referanser
+                 
+Toft, N., Agger, J. F., Houe, H., & Bruun, J. (2004). Measures of disease frequency. In H. Houe, A. K. Ersbøll, & N. Toft (Eds.), Introduction to Veterinary Epidemiology (pp. 77–93). Frederiksberg, Denmark: Biofolia.
+                 
+Bang Jensen, B., Qviller, L. & Toft, N. (2020). Spatio-temporal variations in mortality during the seawater production phase of Atlantic salmon (Salmo salar) in Norway. J. Fish Dis. 43, 445–457.
+                 
+#### Kontakt
+                 
+Dersom du har spørsmål eller kommentarer til tabellene, vennligst ta kontakt med  
+*[Edgar Brun](mailto:edgar.brun@vetinst.no)*, (avdelingsdirektør, fiskehelse og fiskevelferd).
