01443b31300172572a4df0324cf48b9e01d9f82f
novica <nnovica@gmail.com>
2025-03-21 13:29:49 +0100

    fix: resolves #46

--- app.R
+++ app.R
@@ -1160,23 +1160,28 @@ server <- function(input, output) {
             x = ~area, y = ~`2024`, name = "2024", type = 'scatter',
             mode = "markers", marker = list(color = "#253494"),
             hoverinfo = "text", text = ~paste("Område: ", area, "<br>",
-                                              "Prosent: ", `2023`,"<br>")) %>%
+                                              "Prosent: ", `2024`,"<br>",
+                                              "Aar: 2024")) %>%
       add_trace(x = ~area, y = ~`2023`, name = "2023",type = 'scatter',
                 mode = "markers", marker = list(color = "#2c7fb8"),
                 hoverinfo = "text", text = ~paste("Område: ", area, "<br>",
-                                                  "Prosent: ", `2023`,"<br>")) %>%
+                                                  "Prosent: ", `2023`,"<br>",
+                                                  "Aar: 2023")) %>%
       add_trace(x = ~area, y = ~`2022`, name = "2022",type = 'scatter',
                 mode = "markers", marker = list(color = "#41b6c4"),
                 hoverinfo = "text", text = ~paste("Område: ", area, "<br>",
-                                                  "Prosent: ", `2022`,"<br>")) %>%
+                                                  "Prosent: ", `2022`,"<br>",
+                                                  "Aar: 2022")) %>%
       add_trace(x = ~area, y = ~`2021`, name = "2021",type = 'scatter',
                 mode = "markers", marker = list(color = "#a1dab4"),
                 hoverinfo = "text", text = ~paste("Område: ", area, "<br>",
-                                                  "Prosent: ", `2021`,"<br>")) %>%
+                                                  "Prosent: ", `2021`,"<br>",
+                                                  "Aar: 2021")) %>%
       add_trace(x = ~area, y = ~`2020`, name = "2020",type = 'scatter',
                 mode = "markers", marker = list(color = '#feb24c'),
                 hoverinfo = "text", text = ~paste("Område: ", area, "<br>",
-                                                  "Prosent: ", `2020`,"<br>")) %>%
+                                                  "Prosent: ", `2020`,"<br>",
+                                                  "Aar: 2020")) %>%
       layout(title = "", 
              annotations=list(yref='paper',xref="paper",y=1.09,x=.2, text="Velg år:",showarrow=F, font=list(size=14,face="bold")),
              legend = list(orientation = "h", x= .25, y = 1.1),
