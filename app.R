b9461708a13a1d22103fe9c95e1ff4637c817d0f
novica <nnovica@gmail.com>
2025-07-18 08:58:40 +0200

    local exec

--- app.R
+++ app.R
@@ -1,3 +1,7 @@
 shiny::runApp(appDir = getwd())
 #local
+#options(shiny.port = 5555)
+##options(shiny.fullstacktrace = TRUE)
+#options(browser = "/usr/bin/firefox")
+#options(launch.browser = TRUE)
 #shiny::runApp(appDir = getwd(), launch.browser = TRUE, port = 1234)
