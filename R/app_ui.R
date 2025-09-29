867c03f6ac14e27b1206d627105f232d6ffb51bb
novica <nnovica@gmail.com>
2025-09-29 18:20:44 +0200

    main app ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -8,7 +8,12 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic
+    shiny::tags$head(
+      meta_block(),
+      tag_manager(), 
+      shiny::tags$html(lang = "nb-NO")
+    ),
+    
     div(
       class = "d-flex flex-column p-4 gap-3", 
       style = "background-color: #d7f4ff",
@@ -19,7 +24,7 @@ app_ui <- function(request) {
                         style = "height:64px;", 
                         div(class = "container",
                             tags$img(
-                              src = app_sys("app/www/vetinst-logo-no.svg"),
+                              src = "www/vetinst-logo-no.svg",
                               alt = "Veterinærinstituttets logo",
                               style = "height:64px;",
                             ))
@@ -33,8 +38,29 @@ app_ui <- function(request) {
                             `aria-label` = "1",
                           ),
                           shiny::includeMarkdown(app_sys("app/www/header_text.md"))
-          )))
-  )
+          ))),
+    shiny::tags$div(id = "main-content", role="region", `aria-label`= "main",
+                    bslib::page_navbar(
+                      title = "",
+                      footer = tags$footer(
+                        shiny::tags$div(role="region", `aria-label`= "footer",
+                                        shiny::includeHTML(app_sys("app/www/footer.html"))),
+                      ),
+                      id = "navbar",
+                      theme = bslib::bs_theme(primary = "#d7f4ff"),
+                      header = shiny::tagList(
+                        shiny::div(class = "container",
+                                   style = "padding: 1.5rem;",
+                                   shiny::uiOutput("tab_title"),
+                                   shiny::tags$br(),
+                                   shiny::uiOutput("top_bar"),
+                        )),
+                      
+                      #### Tab 1: top level tab monthly mortality ####
+                      bslib::nav_panel(
+                        "Månedlig dødelighet %",
+                        value = "monthly_mortality",  mod_monthly_mortality_ui("monthly_mortality_1"))
+  )))
 }
 
 #' Add external Resources to the Application
@@ -51,16 +77,13 @@ golem_add_external_resources <- function() {
     app_sys("app/www")
   )
 
-  tags$head(
-    favicon(rel = "shortcut icon", resources_path = app_sys("app/www"), ico =  "favicon.png"),
+  shiny::tags$head(
+    favicon(ext = "png"),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "laksetap"
-    ),
-    head_block(),
-    meta_block(),
-    tag_manager()
-    
+      app_title = "Laksetap - Statistikk over tap og dødelighet av laks og regnbueørret i sjøfasen"
+    )
+
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
   )
