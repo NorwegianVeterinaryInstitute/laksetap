7eee9b0c26a72943e559f8335b418c0b3e0d27d8
novica <nnovica@gmail.com>
2025-09-29 18:19:52 +0200

    remove data loading

--- R/app_server.R
+++ R/app_server.R
@@ -5,14 +5,11 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  
+
   #### Data ####
-  laksetap_board <- pins::board_connect()
-  
-  losses <- pins::pin_read(laksetap_board, "vi2451/losses_and_mortality_yearly_data")
-  losses_monthly_data <- pins::pin_read(laksetap_board, "vi2451/losses_monthly_data")
-  mortality_rates_monthly_data <- pins::pin_read(laksetap_board, "vi2451/mortality_rates_monthly_data")
-  mortality_cohorts_data <- pins::pin_read(laksetap_board, "vi2451/mortality_cohorts_data")
-  
-  
+  losses <- getOption("losses")  
+  losses_monthly_data <- getOption("losses_monthly_data")
+  mortality_cohorts_data <- getOption("mortality_cohorts_data")
+  mortality_rates_monthly_data <- getOption("mortality_rates_monthly_data")
+
 }
