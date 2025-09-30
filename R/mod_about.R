4aceb3fd414450fa3b93a3d70b99f2070e6c4262
novica <nnovica@gmail.com>
2025-09-30 11:13:00 +0200

    fix mod about

--- R/mod_about.R
+++ R/mod_about.R
@@ -4,37 +4,34 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_about_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    bslib::nav_panel(
-      "Om statistikken",
-      value = "about",
-      div(class = "container",
-          style = "padding-left: 1.5rem",
-          column(
-            width = 9,
-            shiny::includeMarkdown("app/www/about.md")
-          ))
+    div(
+      class = "container",
+      style = "padding-left: 1.5rem",
+      column(
+        width = 9,
+        shiny::includeMarkdown(app_sys("app/www/about.md"))
+      )
     )
   )
 }
-    
+
 #' about Server Functions
 #'
-#' @noRd 
-mod_about_server <- function(id){
-  moduleServer(id, function(input, output, session){
+#' @noRd
+mod_about_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
- 
   })
 }
-    
+
 ## To be copied in the UI
 # mod_about_ui("about_1")
-    
+
 ## To be copied in the server
 # mod_about_server("about_1")
