0563ec7f1514f9e2e2d013522bf62bca9b9989ba
novica <nnovica@gmail.com>
2025-10-01 15:21:29 +0200

    formatting

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -1,11 +1,10 @@
-select_year <- function(id, multiple = F, resolution = "m"){
-  
+select_year <- function(id, multiple = F, resolution = "m") {
   if (multiple) {
     text = "Velg flere år:"
   } else {
     text = "Velg år:"
   }
-  
+
   if (resolution == "m") {
     selection <- c(
       "2025" = 2025,
@@ -15,10 +14,10 @@ select_year <- function(id, multiple = F, resolution = "m"){
       "2021" = 2021,
       "2020" = 2020
     )
-    
+
     selected <- c(2025)
   }
-  
+
   if (resolution == "y") {
     selection <- c(
       "2024" = 2024,
@@ -27,17 +26,17 @@ select_year <- function(id, multiple = F, resolution = "m"){
       "2021" = 2021,
       "2020" = 2020
     )
-    
+
     selected <- c(2024)
   }
-  
+
   selectInput(
     id,
     text,
     selection,
-    selected = selected, 
+    selected = selected,
     multiple = multiple
-  ) 
+  )
 }
 
 
@@ -70,21 +69,37 @@ select_months <- function(id, multiple = T) {
 my_palette <- c("#FF5447", "#59CD8B", "#FFC6C2", "#1C4FB9")
 
 my_palette_long <- c(
-  "#FF5447", "#59CD8B", "#95D9F3", "#1C4FB9", "#FFC6C2",
-  "#BCEED1", "#D7F4FF", "#C7D9FF", "#F7FDFF", "#091A3E",
-  "#CC0E00", "#288550", "#1FC0FF", "#6892E8", "#BD990A",
-  "#969FB1", "#D3DFF8", "#C0ECD3", "#0076A3", "#F5D34A"
+  "#FF5447",
+  "#59CD8B",
+  "#95D9F3",
+  "#1C4FB9",
+  "#FFC6C2",
+  "#BCEED1",
+  "#D7F4FF",
+  "#C7D9FF",
+  "#F7FDFF",
+  "#091A3E",
+  "#CC0E00",
+  "#288550",
+  "#1FC0FF",
+  "#6892E8",
+  "#BD990A",
+  "#969FB1",
+  "#D3DFF8",
+  "#C0ECD3",
+  "#0076A3",
+  "#F5D34A"
 )
 
 my_palette_named <- c(
-  "Norge" =   "#FF5447", 
-  "Agder" = "#59CD8B", 
-  "Rogaland" = "#95D9F3", 
-  "Vestland" = "#1C4FB9", 
+  "Norge" = "#FF5447",
+  "Agder" = "#59CD8B",
+  "Rogaland" = "#95D9F3",
+  "Vestland" = "#1C4FB9",
   "Møre og Romsdal" = "#FFC6C2",
   "Trøndelag" = "#BCEED1",
   "Nordland" = "#D7F4FF",
-  "Troms" = "#C7D9FF", 
+  "Troms" = "#C7D9FF",
   "Finnmark" = "#091A3E",
   "1 & 2" = "#CC0E00",
   "3" = "#288550",
@@ -93,7 +108,7 @@ my_palette_named <- c(
   "6" = "#BD990A",
   "7" = "#969FB1",
   "8" = "#D3DFF8",
-  "9" = "#C0ECD3", 
+  "9" = "#C0ECD3",
   "10" = "#0076A3",
   "11" = "#F5D34A",
   "12 & 13" = "#59CD8B"
