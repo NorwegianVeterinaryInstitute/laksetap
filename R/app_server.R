207d838a8913d8a7288867d3f432fa8b673c4b13
novica <nnovica@gmail.com>
2025-10-02 16:59:17 +0200

    move data from main server

--- R/app_server.R
+++ R/app_server.R
@@ -6,7 +6,7 @@
 #' @noRd
 app_server <- function(input, output, session) {
   #### Data ####
-  losses <- getOption("losses")
+
   losses_monthly_data <- getOption("losses_monthly_data")
   mortality_cohorts_data <- getOption("mortality_cohorts_data")
 
