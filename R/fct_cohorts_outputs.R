aedd1ec04464e79524e9f517d7b613234ac9b67a
novica <nnovica@gmail.com>
2025-10-06 14:58:37 +0200

    functions for cohorts outputs

---
+++ R/fct_cohorts_outputs.R
@@ -0,0 +1,73 @@
+#' plot_cohorts_output
+#'
+#' @description A function to make the plot for cohort mortality
+#'
+#' @param dat A data frame with cohort mortality data
+#'
+#' @return A plotly object.
+#'
+#' @noRd
+plot_cohorts_output <- function(dat, year) {
+  p <- dat |>
+    ggplot() +
+    geom_segment(
+      aes(
+        color = as.numeric(factor(area)),
+        x = area,
+        xend = area,
+        y = q1,
+        yend = q3
+      ),
+      linewidth = 10
+    ) +
+    scale_color_gradient(low = "#C7D9FF", high = "#1C4FB9") +
+    geom_point(
+      aes(x = area, y = median, group = year, text = tooltip),
+      linewidth = 1,
+      fill = "black",
+      stroke = 0.2
+    ) +
+    geom_text(
+      aes(x = area, y = median, group = year, label = area),
+      nudge_y = 1
+    ) +
+    labs(
+      title = "Fullførte produksjonssykluser (>= 8 måneder)",
+      x = year,
+      y = "Dødelighet %"
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(), legend.position = "none") +
+    guides(fill = "none")
+
+  plotly::ggplotly(p, tooltip = "text") |>
+    plotly::config(displaylogo = FALSE, modeBarButtons = list(list("toImage")))
+}
+
+
+table_cohorts_output <- function(dat) {
+  dat |>
+    DT::datatable(
+      rownames = FALSE,
+      colnames = c(
+        "År",
+        "Område",
+        "1 Kvartil %",
+        "Median %",
+        "3 Kvartil %"
+      ),
+      selection = (list(
+        mode = "multiple",
+        selected = "all",
+        target = "column"
+      )),
+      options = list(
+        sDom = '<"top">lrt<"bottom">ip',
+        autoWidth = FALSE,
+        scrollX = FALSE,
+        language = list(
+          url = "//cdn.datatables.net/plug-ins/2.0.1/i18n/no-NB.json"
+        )
+      )
+    )
+}
