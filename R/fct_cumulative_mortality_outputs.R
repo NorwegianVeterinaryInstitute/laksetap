3d938900c8832c7bca20e72d4851f0027544ad7b
novica <nnovica@gmail.com>
2025-11-18 13:43:06 +0100

    feat: functions for cumulative mortality outputs

---
+++ R/fct_cumulative_mortality_outputs.R
@@ -0,0 +1,83 @@
+#' cumulative_mortality_plot
+#'
+#' @description A function to make the monthly mortality plot.
+#'
+#' @return A ggplot2 object.
+#'
+#' @noRd
+cumulative_mortality_plot <- function(dat) {
+  # Colors #
+  vi_palette_named <- getOption("vi_palette_named")
+  labels <- golem::get_golem_options(which = "labels")
+
+  dat |>
+    ggplot2::ggplot() +
+    aes(
+      x = date,
+      y = mean,
+      group = region,
+      text = paste0(
+        "Mean: ",
+        mean,
+        "<br>LCI: ",
+        lci,
+        "<br>UCI: ",
+        uci
+      )
+    ) +
+    labs(
+      x = labels$output_functions$mortality_plot_label_x,
+      y = labels$output_functions$mortality_plot_label_y
+    ) +
+    geom_line(aes(
+      color = factor(region)
+    )) +
+    geom_ribbon(
+      aes(
+        ymin = .data$lci,
+        ymax = .data$uci,
+        fill = factor(region)
+      ),
+      linetype = 0,
+      alpha = 0.1,
+      show.legend = FALSE
+    ) +
+    theme_minimal() +
+    scale_color_manual(values = vi_palette_named) +
+    scale_fill_manual(values = vi_palette_named) +
+    guides(
+      col = guide_legend(
+        title = labels$output_functions$mortality_plot_label_title
+      ),
+      fill = "none"
+    )
+}
+
+#' monhtly_mortality_table
+#'
+#' @description A function to make the data table.
+#'
+#' @return A DT object.
+#'
+#' @noRd
+cumulative_mortality_table <- function(dat) {
+  labels <- golem::get_golem_options(which = "labels")
+  DT::datatable(
+    dat,
+    rownames = F,
+    colnames = labels$output_functions$table_colnames,
+    selection = (list(
+      mode = "multiple",
+      selected = "all",
+      target = "column"
+    )),
+    options = list(
+      sDom = '<"top">lrt<"bottom">ip',
+      autoWidth = FALSE,
+      scrollX = FALSE,
+      language = list(
+        url = "//cdn.datatables.net/plug-ins/2.0.1/i18n/no-NB.json"
+      )
+    )
+  )
+}
