e3ae68d7dc61a3a5505e7740ecd9bf798d1af4b1
novica <nnovica@gmail.com>
2025-11-19 12:38:47 +0100

    new input helpers for cumulative mortality

--- R/fct_input_helpers.R
+++ R/fct_input_helpers.R
@@ -126,6 +126,132 @@ render_input_for_mortality_month_table <- function(ns, dat, geo_group) {
   }
 }
 
+#' render_input_for_cumulative_mortality_plot
+#'
+#' @param ns namespace function
+#' @param dat dataframe
+#' @param geo_group "area", "county" or "country"
+#'
+#' @returns a taglist to be rendered in the UI
+#' for the plot
+#'
+#' @noRd
+render_input_for_cumulative_mortality_plot <- function(ns, dat, geo_group) {
+  labels <- golem::get_golem_options(which = "labels")
+  
+  region <- c(
+    as.character(unique(dat$region[dat$geo_group == geo_group])),
+    as.character(unique(dat$region[dat$geo_group == 'country']))
+  )
+  
+  if (geo_group == "country") {
+    shiny::tagList(
+      shiny::fluidRow(
+        shiny::column(
+          width = 6,
+          select_year(
+            ns("select_year_cumulative_mortality"),
+            dat,
+            multiple = FALSE
+          )
+        )
+      )
+    )
+  } else {
+    shiny::tagList(
+      shiny::fluidRow(
+        shiny::column(
+          width = 4,
+          select_year(
+            ns("select_year_cumulative_mortality"),
+            dat,
+            multiple = FALSE
+          )
+        ),
+        shiny::column(
+          width = 4,
+          shiny::selectInput(
+            ns("select_region_cumulative_mortality_plot"),
+            label = labels$functions$select_multiple_places,
+            region,
+            selected = c(as.character(unique(dat$region[
+              dat$geo_group == 'country'
+            ]))),
+            multiple = TRUE
+          )
+        )
+      )
+    )
+  }
+}
+
+#' render_input_for_cumulative_mortality_table
+#'
+#' @param ns namespace function
+#' @param dat dataframe
+#' @param geo_group "area", "county" or "country"
+#'
+#' @returns a taglist to be rendered in the UI
+#' for the table
+#'
+#' @noRd
+render_input_for_cumulative_mortality_table <- function(ns, dat, geo_group) {
+  labels <- golem::get_golem_options(which = "labels")
+  
+  region <- c(as.character(unique(dat$region[dat$geo_group == geo_group])))
+  
+  if (geo_group == "country") {
+    shiny::tagList(
+      shiny::fluidRow(
+        shiny::column(
+          width = 6,
+          select_year(
+            ns("select_years_cumulative_mortality_table"),
+            dat,
+            multiple = TRUE
+          )
+        # ),
+        # shiny::column(
+        #   width = 4,
+        #   select_months(
+        #     id = ns("select_months_cumulative_mortality_table")
+        #   )
+        )
+      )
+    )
+  } else {
+    shiny::tagList(
+      shiny::fluidRow(
+        shiny::column(
+          width = 6,
+          select_year(
+            ns("select_years_cumulative_mortality_table"),
+            dat,
+            multiple = TRUE
+          )
+        # ),
+        # shiny::column(
+        #   width = 4,
+        #   select_months(
+        #     id = ns("select_months_cumulative_mortality_table"),
+        #  )
+        ),
+        shiny::column(
+          width = 6,
+          shiny::selectizeInput(
+            ns("select_region_cumulative_mortality"),
+            label = labels$functions$select_multiple_places,
+            region,
+            selected = region,
+            multiple = TRUE
+          )
+        )
+      )
+    )
+  }
+}
+
+
 #' render_input_for_mortality_year
 #' function renders input for both plot and table
 #'
