8e0d54803bf70af65a2a818d4bf9aeff8f123630
novica <nnovica@gmail.com>
2025-11-21 11:52:59 +0100

    Revert "track button downloads"
    This reverts commit 1a036f7a52642858f67b6a1baf1303f50a75b5ae.

--- R/mod_about.R
+++ R/mod_about.R
@@ -52,19 +52,11 @@ mod_about_ui <- function(id) {
               ),
               shiny::downloadButton(
                 ns("download_csv"),
-                label = labels$modules$download_button_csv,
-                
-                onclick = "var dataset = document.querySelector('input[name=\"about_1-which_dataset\"]:checked').value;
-                          console.log('Download button clicked. Format: csv. Dataset:', dataset);"
-              
+                label = labels$modules$download_button_csv
               ),
               shiny::downloadButton(
                 ns("download_json"),
-                label = labels$modules$download_button_json,
-                
-                onclick = "var dataset = document.querySelector('input[name=\"about_1-which_dataset\"]:checked').value;
-                          console.log('Download button clicked. Format: json. Dataset:', dataset);"
-                
+                label = labels$modules$download_button_json
               )
             ),
             shiny::br(),
