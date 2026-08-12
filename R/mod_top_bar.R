b16f7b9110c004765b8416ae3c1ff055ba96f924
novica <nnovica@gmail.com>
2026-08-12 14:11:49 +0200

    chore: formatting.

--- R/mod_top_bar.R
+++ R/mod_top_bar.R
@@ -31,7 +31,9 @@ mod_top_bar_server <- function(id) {
     names(geo_group_choices) <- labels$input_functions$geo_group
 
     output$top_bar <- shiny::renderUI({
-      if (!(session$userData$active_tab() %in% c("about", "about_explanation"))) {
+      if (
+        !(session$userData$active_tab() %in% c("about", "about_explanation"))
+      ) {
         shiny::tagList(
           shiny::fluidRow(
             shiny::column(
